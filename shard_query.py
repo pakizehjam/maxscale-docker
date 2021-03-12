f159d7b07b1fc9d2f651b7a58970504bb94188c8
Mohammad Pakizeh Jam <61758931+pakizehjam@users.noreply.github.com>
2021-03-12 00:15:56 -0800

    Create shard_query.py

---
+++ shard_query.py
@@ -0,0 +1,35 @@
+#Script will query both tables in the databaseIt will output the following information:
+#1.The last 10 rows of zipcodes_one
+#2.The first 10 rows of zipcodes_two
+#3.The largest zipcodenumber in zipcodes_one
+#4.The smallest zipcode_numberin    zipcodes_two
+import pymysql
+
+db = pymysql.connect(host="10.0.0.210", port=4000, user="maxuser", passwd="maxpwd")
+cursor = db.cursor()
+
+print('The last 10 rows of zipcodes_one are:')
+cursor.execute("SELECT * FROM zipcodes_one.zipcodes_one LIMIT 9990,10;")
+results = cursor.fetchall()
+for result in results:
+    print(result)
+
+print('The first 10 rows of zipcodes_two are:')
+cursor.execute("SELECT * FROM zipcodes_two.zipcodes_two LIMIT 10")
+results = cursor.fetchall()
+for result in results:
+    print(result)
+
+print('The largest zipcode number in zipcodes_one is:')
+cursor = db.cursor()
+cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one ORDER BY Zipcode DESC LIMIT 1;")
+results = cursor.fetchall()
+for result in results:
+    print(result)
+
+print('The smallest zipcode number in zipcodes_two is:')
+cursor = db.cursor()
+cursor.execute("SELECT Zipcode FROM zipcodes_two.zipcodes_two ORDER BY Zipcode ASC LIMIT 1")
+results = cursor.fetchall()
+for result in results:
+    print(result)
